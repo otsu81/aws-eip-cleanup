490d93af6f8e99c5ee6c6847e6d841241748bf45
otsu81 <c@otsu.se>
2021-03-02 17:09:30 +0100

    refactor

--- src/getRoleCreds.js
+++ src/getRoleCreds.js
@@ -1,19 +1,18 @@
 const AWS = require('aws-sdk');
 
-let getRoleCredentials = async (accountId, roleName, sessionName) => {
+async function getRoleCredentials (accountId, roleName, sessionName) {
     // Set the region
     AWS.config.update({region: process.env.DEFAULT_REGION});
     const arn = `arn:aws:iam::${accountId}:role/${roleName}`;
-    // const arn = 'arn:aws:iam::' + accountId + ':role/' + roleName
-    var roleToAssume = {RoleArn: arn,
-                        RoleSessionName: sessionName,
-                        DurationSeconds: 900,};
+    const roleToAssume = {
+        RoleArn: arn,
+        RoleSessionName: sessionName,
+        DurationSeconds: 900
+    };
 
     // Create the STS service object
-    var roleCreds;
-    var sts = new AWS.STS();
-    const response = await sts.assumeRole(roleToAssume).promise();
-    return response;
+    const sts = new AWS.STS();
+    return await sts.assumeRole(roleToAssume).promise();
 }
 
-module.exports = getRoleCredentials;
+exports.getRoleCredentials = getRoleCredentials;
